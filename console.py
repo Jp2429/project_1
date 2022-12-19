7b7249032fb5429a88ceb72fbc69268084aeff3c
John Panton <johnpanton29@outlook.com>
2022-12-19 10:59:30 +0000

    created console with some input data

--- console.py
+++ console.py
@@ -5,4 +5,21 @@ from models.book import Book
 import repositories.author_repository as author_repository
 import repositories.book_repository as book_repository
 
-# "Game Of Thrones,Story about thrones,14,3,7,Fantasy"
+
+book_1=Book("Game Of Thrones 1","Story about thrones",14,3,7,"Fantasy")
+book_1_results=book_repository.save(book_1)
+
+book_2=Book("Harry Potter and the Philosophers Stone","Wizards",9,4,6,"Fantasy")
+book_2_results=book_repository.save(book_2)
+
+book_3=Book("Harry Potter and the Chamber of Secrets","Wizards",16,7,11,"Fantasy")
+book_3_results=book_repository.save(book_3)
+
+author_1=Author("George R.R","Martin",74)
+author_1_results=author_repository.save(author_1)
+
+author_2=Author("J.k","Rowling",57)
+author_2_results=author_repository.save(author_2)
+
+
+# 
