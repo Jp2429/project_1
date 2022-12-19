e9a8d86c0896a526f959efae40f457ac3f62d306
John Panton <johnpanton29@outlook.com>
2022-12-19 11:05:35 +0000

    fixed issue with repository

--- repositories/author_repository.py
+++ repositories/author_repository.py
@@ -4,7 +4,7 @@ from models.book import Book
 from models.author import Author
 
 def save(author):
-    sql="INSERT INTO author (first_name,last_name,age) VALUES (%s,%s,%s)"
+    sql="INSERT INTO authors (first_name,last_name,age) VALUES (%s,%s,%s) RETURNING *"
     values=[author.first_name,author.last_name,author.age]
     query_results=run_sql(sql,values)
     id =query_results[0]['id']
--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -4,8 +4,8 @@ from models.book import Book
 from models.author import Author
 
 def save(book):
-    sql="INSERT INTO books(title,description,stock_quantity,buying_cost,selling_price,genre) VALUES(%s,%s,%s,%s,%s,%s)"
-    values= [book.title,book.description,book.stock_quantity,book.buying_price,book.selling_price,book.genre]
+    sql="INSERT INTO books(title,description,stock_quantity,buying_cost,selling_price,genre) VALUES(%s,%s,%s,%s,%s,%s)RETURNING *"
+    values= [book.title,book.description,book.stock_quantity,book.buying_cost,book.selling_price,book.genre]
     query_results=run_sql(sql,values)
     id =query_results[0]['id']
     book.id=id
