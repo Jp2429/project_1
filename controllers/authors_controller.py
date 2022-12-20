fd22aa458f2ee8eb6b462cdd51c02b41366c7b6e
John Panton <johnpanton29@outlook.com>
2022-12-20 09:57:11 +0000

    created select_all,delete and select routes for authors

--- controllers/authors_controller.py
+++ controllers/authors_controller.py
@@ -12,3 +12,14 @@ authors_blueprint = Blueprint("authors", __name__)
 def index():
     authors = author_repository.select_all()
     return render_template("authors/index.html", all_authors=authors)
+
+@authors_blueprint.route("/authors/<id>")
+def show_book(id):
+    author=author_repository.select(id)
+    return render_template("/authors/show_author.html",author=author)
+
+# delete
+@authors_blueprint.route("/authors/<id>/delete", methods=['POST'])
+def destroy(id):
+    author_repository.delete(id)
+    return redirect ("/authors")
